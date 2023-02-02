11540cdddfcf769750a498f48a5049f15c53e638
kamarini <kama0549@stud.kea.dk>
2023-02-02 22:09:00 +0100

    finally is working

--- script.js
+++ script.js
@@ -15,19 +15,25 @@ const myArray = [
 const randomComputerChoice = myArray[Math.floor(Math.random() * 3)];
 // console.log(randomComputerChoice);
 
-
-function playerChoice(){
-    document.querySelector("button.rock").addEventListener("click", shaking);
-    document.querySelector("button.scissors").addEventListener("click", shaking);
-    document.querySelector("button.paper").addEventListener("click", shaking);
+function shake_scissors(){
+    shaking('scissors')
 }
 
-function shaking(){
+
+
+function playerChoice(){
+    document.querySelector("button.rock").addEventListener("click", () => {shaking('rock')});
+    document.querySelector("button.scissors").addEventListener("click",  shake_scissors);
+    document.querySelector("button.paper").addEventListener("click", () => {shaking('paper')});
+}
+
+function shaking(hand){
+
     console.log('shaking')
     document.querySelector("div#player1").classList.add("shake");
     document.querySelector("div#player2").classList.add("shake");
     document.querySelector("div#player1").addEventListener("animationend", changeComputerHand);
-    document.querySelector("div#player2").addEventListener("animationend", changeUserHand);
+    document.querySelector("div#player2").addEventListener("animationend", () => {changeUserHand(hand)});
 }
 
 function changeComputerHand(){
@@ -36,28 +42,48 @@ function changeComputerHand(){
     document.querySelector("div#player1").classList.add(randomComputerChoice);
 }
 
-function changeUserHand(){
+function changeUserHand(hand){
     console.log('change user hand');
     document.querySelector("div#player2").classList.remove("shake");
-    document.querySelector("div#player2").classList.add("paper");
-    determineWinner();
+    document.querySelector("div#player2").classList.add(hand);
+    determineWinner(hand);
 }
 
-const user = "paper";
+// const user = "paper";
 const computer = randomComputerChoice;
 // console.log(user);
 // console.log(computer);
 
-function determineWinner(){
-    if(user === computer) {
+function determineWinner(hand){
+    if(hand === computer) {
         showDraw();
     }
-    else if (computer == 'rock'){
-        showWin();
-    } 
-    else{
+    else if (hand == 'rock'){
+        if(computer =='scissors'){
+            showWin();
+        }
+       else {
         showLose();
+       }  
+    
+    } 
+    else if(hand == 'scissors'){
+        if(computer =='paper'){
+            showWin();
+        }
+       else {
+        showLose();
+       }  
+       
     }
+    else if(hand == 'paper'){
+        if(computer =='rock'){
+            showWin();
+        }
+       else {
+        showLose();
+       }  
+}
 }
 
 function showWin(){
